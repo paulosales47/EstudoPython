5d44d5a870541c7fa2ed63e13faf0b011c735e59
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-04-15 11:33:01 -0300

    Variavel booleanas, médoto find e for sobre string

---
+++ Curso 2/estudo2.py
@@ -0,0 +1,35 @@
+# BOOL
+
+# var_bool = True
+# print(type(var_bool))
+
+# print(bool(0)) # false
+# print(bool("")) # false
+# print(bool(None)) # false
+
+# print(bool(1)) # true
+# print(bool(-100)) # true
+# print(bool(13.5)) # true
+# print(bool("teste")) # true
+
+
+# FIND
+
+# palavra = "Paulo Henrique"
+
+# index = palavra.find("H")
+# print(index) # 6
+
+# index = palavra.find("h")
+# print(index) # -1
+
+# FOR STR
+
+palavra = "Paulo Henrique"
+
+for letra in palavra:
+    print(letra)
+
+
+
+
