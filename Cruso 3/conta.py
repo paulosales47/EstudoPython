7e7ce82193aad1060e052913640a206c5413eff6
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-04-22 14:55:01 -0300

    Configurando parametros opcionais para o construtor

--- Cruso 3/conta.py
+++ Cruso 3/conta.py
@@ -1,12 +1,8 @@
 class Conta:
     
-    def __init__(self, numero, titular, saldo, limite):
+    def __init__(self, numero, titular, saldo, limite= 1000.0):
         print("Construindo objeto.. {}".format(self))
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
-    
-
-    
-    
--- Cruso 3/estudo3.py
+++ Cruso 3/estudo3.py
@@ -10,3 +10,10 @@ from conta import Conta
 #conta = Conta()
 
 # conta = Conta(1, "Paulo Henrique", 1000.0, 5000.0)
+
+
+# PARAMETRO OPCIONAL NO CONSTRUTOR
+
+# conta = Conta(1, "CONTA 1", 1000.0, 5000.0)
+
+# conta2 = Conta(1, "CONTA 2", 1200.0)
