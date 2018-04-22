acaa1f7f8b86d7de1e20a502cfd7a1a22ea68551
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-04-22 14:50:41 -0300

    Utilizando parametros no construtor

--- Cruso 3/estudo3.py
+++ Cruso 3/estudo3.py
@@ -1,5 +1,12 @@
 from conta import Conta
 
-conta = Conta()
+# conta = Conta()
 
-print(conta)
+# print(conta)
+
+
+# TESTANDO CONSTRUTOR
+
+#conta = Conta()
+
+# conta = Conta(1, "Paulo Henrique", 1000.0, 5000.0)
