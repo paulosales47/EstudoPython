65c65d9d9e967fdbcfa6321a64253716ec5a0606
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-04-22 15:56:30 -0300

    Funções e exercicio

--- Cruso 3/estudo3.py
+++ Cruso 3/estudo3.py
@@ -17,3 +17,19 @@ from conta import Conta
 # conta = Conta(1, "CONTA 1", 1000.0, 5000.0)
 
 # conta2 = Conta(1, "CONTA 2", 1200.0)
+
+#FUNÇÕES
+
+# conta3 = Conta(3, "CONTA 3", 500.0, 1500.0)
+
+# conta3.extrato()
+# conta3.deposita(100.0)
+# conta3.extrato()
+
+#EXERCICIO
+
+# from datas import Data
+
+# data = Data(1, 4, 2018)
+# data.formatada()
+
